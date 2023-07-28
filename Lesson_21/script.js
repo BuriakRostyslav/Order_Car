9ecce48c68fa91c1e988f5e53171aa396f92a8ec
BuriakRostyslav <131865677+BuriakRostyslav@users.noreply.github.com>
2023-07-28 15:00:02 +0300

    ls21.1

---
+++ Lesson_21/script.js
@@ -0,0 +1,13 @@
+let num1 = prompt ('Write first numbers');
+let num2 = prompt ('Write second number');
+let sum = num1 + num2;
+let diff = num1 - num2;
+let pr = num1 * num2;
+let div = num1 / num2;
+console.log(sum);
+console.log(diff);
+console.log(pr);
+console.log(div);
+let result = 'Result:' + '\n' + sum + '\n' + diff + '\n' + pr +'\n' + div;
+alert (result); 
+
